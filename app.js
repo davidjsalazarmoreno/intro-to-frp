fcc35f29dd4cbddf258eeb0204b635c02f4d9d0f
David Salazar <djsm777@gmail.com>
2016-11-11 11:03:53 -0400

    Replacing merge with startWith

--- app.js
+++ app.js
@@ -3,16 +3,11 @@ document.addEventListener('DOMContentLoaded', function() {
   var refreshButton = document.querySelector('.refresh');
   var refreshClickStream = Rx.Observable.fromEvent(refreshButton, 'click');
 
-  var requestOnRefreshStream = refreshClickStream.map(function() {
+  var requestStream = refreshClickStream.startWith('click at startup').map(function() {
     var randomOffset = Math.floor(Math.random() * 500);
     console.log(randomOffset);
     return 'https://api.github.com/users?since=' + randomOffset;
   });
-
-  var requestStream = Rx.Observable.merge(
-    requestOnRefreshStream,
-    startupRequestStream
-  );
 
   var responseStream = requestStream.flatMap(function(requestUrl) {
     return Rx.Observable.fromPromise(jQuery.getJSON(requestUrl));
