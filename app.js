28edd7d9e3a693160c7dffe31133cd3a565a8612
David Salazar <djsm777@gmail.com>
2016-11-11 12:09:11 -0400

    working example

--- app.js
+++ app.js
@@ -1,40 +1,76 @@
 document.addEventListener('DOMContentLoaded', function() {
-  var startupRequestStream = Rx.Observable.just('https://api.github.com/users');
   var refreshButton = document.querySelector('.refresh');
   var closeButton1 = document.querySelector('.close1');
-  var closeClickStream = Rx.Observable.fromEvent(close1Button, 'click');
-  var refreshClickStream = Rx.Observable.fromEvent(refreshButton, 'click');
+  var closeButton2 = document.querySelector('.close2');
+  var closeButton3 = document.querySelector('.close3');
 
-  var requestStream = refreshClickStream.startWith('click at startup').map(function() {
+  var refreshClickStream = Rx.Observable.fromEvent(refreshButton, 'click');
+  var close1ClickStream = Rx.Observable.fromEvent(closeButton1, 'click');
+  var close2ClickStream = Rx.Observable.fromEvent(closeButton2, 'click');
+  var close3ClickStream = Rx.Observable.fromEvent(closeButton3, 'click');
+
+  var requestStream = refreshClickStream.startWith('click at startup')
+  .map(function() {
     var randomOffset = Math.floor(Math.random() * 500);
     console.log(randomOffset);
     return 'https://api.github.com/users?since=' + randomOffset;
   });
 
   var responseStream = requestStream.flatMap(function(requestUrl) {
-    return Rx.Observable.fromPromise(jQuery.getJSON(requestUrl));
+    return Rx.Observable.fromPromise( jQuery.getJSON(requestUrl) );
   });
 
-  var suggestion1Stream = responseStream.startWith('startup click').combineLatest(responseStream,
-    return function(click, listUsers) {
-      // get one random user from list
-      return listUsers[ Math.floor(Math.random()*listUsers.length) ];
+  function createSuggestionStream(closeClickStream) {
+    return closeClickStream.startWith('startup click')
+        .combineLatest(responseStream,
+            function(click, listUsers) {
+                return listUsers[Math.floor(Math.random()*listUsers.length)];
+            }
+        )
+        .merge(
+            refreshClickStream.map(function(){
+                return null;
+            })
+        )
+        .startWith(null);
+  }
+
+  var suggestion1Stream = createSuggestionStream(close1ClickStream);
+  var suggestion2Stream = createSuggestionStream(close2ClickStream);
+  var suggestion3Stream = createSuggestionStream(close3ClickStream);
+
+
+// Rendering ---------------------------------------------------
+  function renderSuggestion(suggestedUser, selector) {
+    var suggestionEl = document.querySelector(selector);
+
+    if ( suggestedUser === null ) {
+      suggestionEl.style.visibility = 'hidden';
+    } else {
+      suggestionEl.style.visibility = 'visible';
+      var usernameEl = suggestionEl.querySelector('.username');
+
+      usernameEl.href = suggestedUser.html_url;
+      usernameEl.textContent = suggestedUser.login;
+      var imgEl = suggestionEl.querySelector('img');
+      imgEl.src = '';
+      imgEl.src = suggestedUser.avatar_url;
     }
-  ).merge(
-    refreshClickStream.map(function() { return null; })
-  ).startWith( null );
+
+  }
 
   responseStream.subscribe(function(response) {
     console.log(response);
   });
 
   suggestion1Stream.subscribe(function(suggestion) {
-    if ( suggestion === null ) {
-      // Hide first suggestion DOM Element
-    } else {
-      // Show the first suggestion DOM element
-      // and render data
-      console.log(suggestion);
-    }
+    renderSuggestion(suggestion, '.suggestion1');
   });
+  suggestion2Stream.subscribe(function(suggestion) {
+    renderSuggestion(suggestion, '.suggestion2');
+  });
+  suggestion3Stream.subscribe(function(suggestion) {
+    renderSuggestion(suggestion, '.suggestion3');
+  });
+
 });
